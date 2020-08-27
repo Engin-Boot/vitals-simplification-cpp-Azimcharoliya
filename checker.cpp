949eae7135726f970676c4095389fec7d0583a3a
Azimcharoliya <39900677+Azimcharoliya@users.noreply.github.com>
2020-08-27 17:28:25 +0530

    Added Test Cases

--- checker.cpp
+++ checker.cpp
@@ -1,17 +1,33 @@
 #include <assert.h>
 
+const int bpmLimits[] = {70,150};
+const int spo2Min = 90;
+const int respRateLimits[] = {30,95};
+
+bool vitalIsOk(float value,int lower,int upper)
+{
+	return (value >= lower && value <= upper);
+}
+
+bool vitalIsOk(float value,int lower)
+{
+	return (value >= lower);
+}
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 90) {
-    return false;
-  } else if(respRate < 30 || respRate > 95) {
-    return false;
-  }
-  return true;
+	return (vitalIsOk(bpm,bpmLimits[0],bpmLimits[1]) && vitalIsOk(spo2,spo2Min) && vitalIsOk(respRate,respRateLimits[0],respRateLimits[1]));
 }
 
 int main() {
+  assert(vitalIsOk(5,2,8)==true);
+  assert(vitalIsOk(5,6,8)==false);
+  assert(vitalIsOk(5,2,4)==false);
+  assert(vitalIsOk(5,2)==true);
+  assert(vitalIsOk(5,10)==false);
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
+  assert(vitalsAreOk(170, 95, 40) == false);
+  assert(vitalsAreOk(80, 85, 40) == false);
+  assert(vitalsAreOk(80, 95, 20) == false);
+  assert(vitalsAreOk(80, 95, 100) == false);
 }
